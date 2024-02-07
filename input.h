6f4c35726f12edc1ac0d9894367b58f514506a00
grabinovich20 <doofrab@gmail.com>
2024-02-06 20:27:08 -0700

    first commit

---
+++ input.h
@@ -0,0 +1,10 @@
+#ifndef INPUT
+#define INPUT
+
+#include <vector>
+#include <iostream>
+
+void readInput(std::vector<std::string> *input, std::string inputFile);
+void makeGraph(std::string inputFile);
+
+#endif
